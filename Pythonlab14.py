8e58b82e1d205f7f4dd99ab32ab2f503dc411097
Fermichell <114905013+Fermichell@users.noreply.github.com>
2024-02-23 15:48:29 -0500

    Add files via upload

---
+++ Pythonlab14.py
@@ -0,0 +1,33 @@
+﻿def factorial(n):
+  """
+  Функція для обчислення факторіала числа.
+
+  Args:
+    n: Ціле число.
+
+  Returns:
+    Факторіал числа n.
+  """
+
+  if n < 0:
+    raise ValueError("n має бути невід'ємним")
+
+  if n == 0:
+    return 1
+
+  else:
+    return n * factorial(n-1)
+
+
+def main():
+  """
+  Функція для демонстрації роботи factorial().
+  """
+
+  number = int(input("Введіть число: "))
+  result = factorial(number)
+  print(f"Факторіал числа {number} дорівнює {result}")
+
+
+if __name__ == "__main__":
+  main()
